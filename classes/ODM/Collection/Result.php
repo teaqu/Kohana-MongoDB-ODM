6211195e8fd0d93003dbc3a17272360724c77a6e
kaamaru <calum@openmailbox.org>
2014-03-14 17:39:12 +0000

    delete old files

--- classes/ODM/Collection/Result.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-/** 
- * The result of ORM_Collection
- *
- * This class has various methods to make development easier
- * 
- * @package ODM
- */
-class ODM_Collection_Result extends Kohana_ODM_Collection_Result {}
